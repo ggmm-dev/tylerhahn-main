4119220a43fdb5fdce5ae6d7bd9781fb3e480261
Tyler Hahn <tyler.hahn@gogeddit.com>
2018-12-30 21:41:31 -0600

    cms

--- src/views/Home.js
+++ src/views/Home.js
@@ -10,15 +10,11 @@ const Wrapper = styled.div`
 `
 
 export default ({ fields }) => {
-  const { title, subtitle, featuredImage, body } = fields
+  const { intro } = fields
   return (
     <main className='Home'>
       <Wrapper>
-        <TextHeadline
-          width='70%'
-          align='left'
-          subheadline='Tyler is a multi-discplinary artist specializing in web development, user experience design and brand identity acting as lead developer at GGMM in Milwaukee, WI. Other active projects include One Hot Pod, Portrait Project, and an untitled board game project amongst other things.'
-        />
+        <TextHeadline width='70%' align='left' subheadline={intro} />
         <Mosaic aData='1' bData='2' cData='3' />
         <Mosaic layout='reverse' aData='1' bData='2' cData='3' />
       </Wrapper>
